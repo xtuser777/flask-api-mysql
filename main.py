54b7335081e5665158b5ec2385c59c40578ed36a
Lucas Oliveira <lucaoxt@gmail.com>
2024-02-12 10:47:53 -0300

    exceptions handler

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask, make_response, jsonify, request
-from mysql.connector import (connection)
+from mysql.connector import (connection, errorcode, Error)
 
 def get_connection():
-    conn = connection.MySQLConnection(
-        host='localhost',
-        user='root',
-        password='root',
-        database='flask_api'
-    )
+    try:
+        conn = connection.MySQLConnection(
+            host='localhost',
+            user='root',
+            password='root',
+            database='flask_api'
+        )
+    except Error as err:
+        conn = None
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
 
     return conn
 
@@ -16,77 +25,121 @@ app = Flask(__name__)
 @app.route('/cars', methods=['GET'])
 def index():
     conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM cars")
-    carsdb = cursor.fetchall()
-    cars = list()
-    for car in carsdb:
-        cars.append(
-            {
-                'id': car[0],
-                'brand': car[1],
-                'model': car[2],
-                'year': car[3]
-            }
-        )
-    cursor.close()
-    conn.close()
+    if conn and conn.is_connected():
+        cars = list()
 
-    return make_response(jsonify(cars), 200)
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM cars")
+            carsdb = cursor.fetchall()
+            for car in carsdb:
+                cars.append(
+                    {
+                        'id': car[0],
+                        'brand': car[1],
+                        'model': car[2],
+                        'year': car[3]
+                    }
+                )
+            cursor.close()
+        except Error as err:
+            print(err)
+            return make_response(jsonify(err.msg), 500)
+        else:
+            conn.close()
+
+        return make_response(jsonify(cars), 200)
+    else:
+        return make_response(jsonify(message='Erro de conexão ao banco.'), 500)
 
 @app.route('/cars/<int:id>', methods=['GET'])
 def show(id: int):
     conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM `cars` WHERE `id` = {id}")
-    carsdb = cursor.fetchone()
-    car = {
-        'id': carsdb[0],
-        'brand': carsdb[1],
-        'model': carsdb[2],
-        'year': carsdb[3]
-    }
+    if conn and conn.is_connected():
+        car = None
 
-    cursor.close()
-    conn.close()
+        try:
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT * FROM `cars` WHERE `id` = {id}")
+            carsdb = cursor.fetchone()
+            car = {
+                'id': carsdb[0],
+                'brand': carsdb[1],
+                'model': carsdb[2],
+                'year': carsdb[3]
+            }
+            cursor.close()
+        except Error as err:
+            print(err)
+            return make_response(jsonify(err.msg), 500)
+        else:
+            conn.close()
 
-    return make_response(jsonify(car), 200)
-
-
+        return make_response(jsonify(car), 200)
+    else:
+        return make_response(jsonify(message='Erro de conexão ao banco.'), 500)
 
 @app.route('/cars', methods=['POST'])
 def create():
     conn = get_connection()
-    car = request.json
-    cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO `cars` (`brand`,`model`,`year`) VALUES ('{car['brand']}','{car['model']}',{car['year']})")
-    conn.commit()
-    cursor.close()
-    conn.close()
+    if conn and conn.is_connected():
+        car = None
 
-    return make_response(jsonify(car), 201)
+        try:
+            car = request.json
+            cursor = conn.cursor()
+            cursor.execute(f"INSERT INTO `cars` (`brand`,`model`,`year`) VALUES ('{car['brand']}','{car['model']}',{car['year']})")
+            conn.commit()
+            cursor.close()
+        except Error as err:
+            print(err)
+            return make_response(jsonify(err.msg), 500)
+        else:
+            conn.close()
+
+        return make_response(jsonify(car), 201)
+    else:
+        return make_response(jsonify(message='Erro de conexão ao banco.'), 500)
 
 @app.route('/cars/<int:id>', methods=['PUT'])
 def update(id: int):
     conn = get_connection()
-    car = request.json
-    cursor = conn.cursor()
-    cursor.execute(f"UPDATE `cars` SET `brand` = '{car['brand']}',`model` = '{car['model']}',`year` = {car['year']} WHERE `id` = {id}")
-    conn.commit()
-    cursor.close()
-    conn.close()
+    if conn and conn.is_connected():
+        car = None
 
-    return make_response(jsonify(car), 200)
+        try:
+            car = request.json
+            cursor = conn.cursor()
+            cursor.execute(f"UPDATE `cars` SET `brand` = '{car['brand']}',`model` = '{car['model']}',`year` = {car['year']} WHERE `id` = {id}")
+            conn.commit()
+            cursor.close()
+        except Error as err:
+            print(err)
+            return make_response(jsonify(err.msg), 500)
+        else:
+            conn.close()
+
+        return make_response(jsonify(car), 200)
+    else:
+        return make_response(jsonify(message='Erro de conexão ao banco.'), 500)
 
 @app.route('/cars/<int:id>', methods=['DELETE'])
 def delete(id: int):
     conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(f"DELETE FROM `cars` WHERE `id` = {id}")
-    conn.commit()
-    cursor.close()
-    conn.close()
+    if conn and conn.is_connected():
+        try:
+            cursor = conn.cursor()
+            cursor.execute(f"DELETE FROM `cars` WHERE `id` = {id}")
+            conn.commit()
+            cursor.close()
+        except Error as err:
+            print(err)
+            return make_response(jsonify(err.msg), 500)
+        else:
+            conn.close()
 
-    return make_response(jsonify(), 204)
-
+        return make_response(jsonify(None), 204)
+    else:
+        return make_response(jsonify(message='Erro de conexão ao banco.'), 500)
+    
 app.run()
